d7a4912008fa7ba5c57df04595d3a16030dfa422
julien-p-code <229524075+julien-p-code@users.noreply.github.com>
2026-09-25 16:40:04 +0200

    refactor(schema): rename Inquiry.guestCount @@check constraint

--- src/prisma/contract.d.ts
+++ src/prisma/contract.d.ts
@@ -34,7 +34,7 @@ import type {
 } from '@prisma/orm-postgres/contract/types';
 
 export type StorageHash =
-  StorageHashBase<'ea281228fb8ae8ac798169e6261477a7b6874da1ddbda12337492b52ab596574'>;
+  StorageHashBase<'9d90495e6e127312e9e041b8fa25d10e7c1808ed53b574fa883bda1eb7582081'>;
 export type ExecutionHash = ExecutionHashBase<string>;
 export type ProfileHash =
   ProfileHashBase<'3916f444a8a17ad749191acf9e08dad97d1a327b88c2f1d45d12f240296aa8b2'>;
